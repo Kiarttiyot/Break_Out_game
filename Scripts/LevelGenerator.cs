651db4a1c94cb230687dccec8287a1bb85babaf6
Yotiki268 <66200028@kmitl.ac.th>
2024-12-13 22:37:22 +0700

    LevelGenerator

---
+++ Scripts/LevelGenerator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector2Int size;
+    public Vector2 offset;
+    public GameObject brickPrefab; //*for include GameObject to use.
+
+    private void Awake()
+    {
+        for(int i = 0; i < size.x;i++)
+        {
+            for (int j = 0; j < size.x;j ++)
+            {
+                GameObject newBrick = Instantiate(brickPrefab,transform);
+                newBrick.transform.position = transform.position+ new Vector3((float)((size.x -1)*.5f -i)*offset.x,j*offset.y,0);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
