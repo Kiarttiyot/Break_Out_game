325106d21acf39432508bd32c53555997e3cd4cd
Yotiki268 <66200028@kmitl.ac.th>
2024-12-13 22:48:12 +0700

    Brick color Renderer

--- Scripts/LevelGenerator.cs
+++ Scripts/LevelGenerator.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     public Vector2Int size;
     public Vector2 offset;
     public GameObject brickPrefab; //*for include GameObject to use.
+    public Gradient gradient;
 
     private void Awake()
     {
@@ -16,6 +17,7 @@ public class NewBehaviourScript : MonoBehaviour
             {
                 GameObject newBrick = Instantiate(brickPrefab,transform);
                 newBrick.transform.position = transform.position+ new Vector3((float)((size.x -1)*.5f -i)*offset.x,j*offset.y,0);
+                newBrick.GetComponent<SpriteRenderer>().color = gradient.Evaluate((float)j/(size.y-1));
             }
         }
     }
